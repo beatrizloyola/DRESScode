42c590351c62dfb4412a33179fbd33e5c60265e7
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-03-25 14:02:54 -0300

    fix: fundo transparente na tela de edição

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dcf$w8eg!hkbsw)!zq$p3c#eiug3!%__hem*yw&4($%r7p@2f@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
 
 # Application definition
