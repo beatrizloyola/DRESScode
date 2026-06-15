ff3d8b3a111564b9f5586c4b42c0f8aa879584d7
Beatriz Loyola <beatrizvasconcelos0802@gmail.com>
2026-06-15 16:48:22 -0300

    test: TestPieceViews

--- wardrobe/tests.py
+++ wardrobe/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from unittest.mock import patch
 from .models import Piece, Outfit
 
 
@@ -118,3 +120,119 @@ class TestAuthViews(TestCase):
         response = self.client.get(self.logout_url)
         self.assertRedirects(response, reverse('landing'))
         self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+
+def _fake_image():
+    return SimpleUploadedFile('shirt.png', b'\x89PNG\r\n', content_type='image/png')
+
+
+class TestPieceViews(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._patcher_save = patch(
+            'cloudinary_storage.storage.MediaCloudinaryStorage._save',
+            return_value='pieces/fake.png',
+        )
+        cls._patcher_url = patch(
+            'cloudinary_storage.storage.MediaCloudinaryStorage.url',
+            return_value='https://res.cloudinary.com/fake/image/upload/pieces/fake.png',
+        )
+        cls._patcher_save.start()
+        cls._patcher_url.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._patcher_save.stop()
+        cls._patcher_url.stop()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='u1@test.com', password='pass123')
+        self.user2 = User.objects.create_user(username='u2@test.com', password='pass123')
+        self.client.force_login(self.user1)
+        self.piece = Piece.objects.create(
+            user=self.user1, name='Camiseta', category='shirt', image='pieces/fake.png'
+        )
+
+    # --- add_piece ---
+
+    def test_add_piece_success(self):
+        response = self.client.post(reverse('add_piece'), {
+            'name': 'Nova Camisa',
+            'category': 'shirt',
+            'image': _fake_image(),
+        })
+        self.assertRedirects(response, reverse('my_pieces'))
+        self.assertTrue(Piece.objects.filter(name='Nova Camisa', user=self.user1).exists())
+
+    def test_add_piece_missing_category(self):
+        count_before = Piece.objects.count()
+        response = self.client.post(reverse('add_piece'), {
+            'name': 'Sem Categoria',
+            'image': _fake_image(),
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Piece.objects.count(), count_before)
+        messages = list(response.context['messages'])
+        self.assertTrue(any('imagem' in str(m).lower() or 'categoria' in str(m).lower() for m in messages))
+
+    def test_add_piece_requires_login(self):
+        self.client.logout()
+        response = self.client.get(reverse('add_piece'))
+        self.assertRedirects(response, f"{reverse('login')}?next={reverse('add_piece')}")
+
+    # --- my_pieces ---
+
+    def test_my_pieces_only_own(self):
+        Piece.objects.create(user=self.user2, name='Calça Alheia', category='pants', image='pieces/fake.png')
+        response = self.client.get(reverse('my_pieces'))
+        self.assertEqual(response.status_code, 200)
+        all_pieces = list(response.context['shirts']) + list(response.context['pants']) + list(response.context['shoes'])
+        self.assertTrue(all(p.user == self.user1 for p in all_pieces))
+
+    def test_my_pieces_search_by_name(self):
+        Piece.objects.create(user=self.user1, name='Calça Jeans', category='pants', image='pieces/fake.png')
+        Piece.objects.create(user=self.user1, name='Tênis Branco', category='shoes', image='pieces/fake.png')
+        response = self.client.get(reverse('my_pieces'), {'q': 'calça'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['total_results'], 1)
+
+    # --- edit_piece ---
+
+    def test_edit_piece_owner(self):
+        response = self.client.post(reverse('edit_piece', args=[self.piece.id]), {
+            'name': 'Camiseta Editada',
+            'category': 'pants',
+        })
+        self.assertRedirects(response, reverse('my_pieces'))
+        self.piece.refresh_from_db()
+        self.assertEqual(self.piece.name, 'Camiseta Editada')
+        self.assertEqual(self.piece.category, 'pants')
+
+    def test_edit_piece_other_user(self):
+        self.client.force_login(self.user2)
+        response = self.client.post(reverse('edit_piece', args=[self.piece.id]), {
+            'name': 'Hack',
+            'category': 'shirt',
+        })
+        self.assertEqual(response.status_code, 404)
+        self.piece.refresh_from_db()
+        self.assertEqual(self.piece.name, 'Camiseta')
+
+    # --- delete_piece ---
+
+    def test_delete_piece_owner(self):
+        response = self.client.post(reverse('delete_piece', args=[self.piece.id]))
+        self.assertRedirects(response, reverse('my_pieces'))
+        self.assertFalse(Piece.objects.filter(id=self.piece.id).exists())
+
+    def test_delete_piece_other_user(self):
+        self.client.force_login(self.user2)
+        response = self.client.post(reverse('delete_piece', args=[self.piece.id]))
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(Piece.objects.filter(id=self.piece.id).exists())
+
+    def test_delete_piece_get_noop(self):
+        self.client.get(reverse('delete_piece', args=[self.piece.id]))
+        self.assertTrue(Piece.objects.filter(id=self.piece.id).exists())
